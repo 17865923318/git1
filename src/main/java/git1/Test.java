6d084dce95fb6490ab25a8d03306819ce37ff12b
csc <17865923318@qq.com>
2018-07-28 11:51:05 +0800

    master 新建了一个test类

---
+++ src/main/java/git1/Test.java
@@ -0,0 +1,18 @@
+package git1;
+
+public class Test {
+	
+	private String name;
+
+	public String getName() {
+		System.out.println("hello very test");
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
